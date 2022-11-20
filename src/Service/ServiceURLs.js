efd6aff0076b43e782da9329aeb5316746118d9d
PangevG <PangevG@gmail.com>
2022-11-20 19:16:29 +0200

    feat/Album service by album Id added

--- src/Service/ServiceURLs.js
+++ src/Service/ServiceURLs.js
@@ -2,3 +2,4 @@ const soursURLs = {
     allAlbumsURL: "https://jsonplaceholder.typicode.com/photos",
     albumByIDURL: "https://jsonplaceholder.typicode.com/photos?albumId="
 }
+export default soursURLs;
