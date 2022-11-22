0382d5d8b65ce6f12b1f6491d8c4b8c2d234775c
PangevG <PangevG@gmail.com>
2022-11-22 20:06:58 +0200

    Feat/Routes and Route added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import HomePage from "./components/HomePage/HomePage";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <HomePage/>
+            <Routes>
+                <Route path="" element={<HomePage/>}/>
+                <Route path="/favorites"></Route>
+            </Routes>
         </div>
     );
 }
