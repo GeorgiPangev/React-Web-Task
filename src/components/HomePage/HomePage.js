e1860f4616a905af4d3cb9660101b04f5b5053b4
PangevG <PangevG@gmail.com>
2022-11-24 18:25:47 +0200

    feat/ Album list created; albums fetching added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,17 @@
+import {useEffect, useState} from "react";
+import {getAlbumByID} from "../../Service/albumsService";
 
 const HomePage = () => {
+    const [albumId, setAlbumId] = useState(1)
+    const [album, setAlbum] = useState([])
+    useEffect(() => {
+        getAlbumByID(albumId).then((albumFromDB) => setAlbum(Array.from(albumFromDB)))
+    }, [albumId])
     return (
         <div>
+
             <h1>Welcome from home page</h1>
         </div>
     );
 };
-
 export default HomePage;
---
+++ src/components/HomePage/albumsList.js
@@ -0,0 +1,21 @@
+const ALBUMS = [
+    {
+        label: 'First',
+        value: 1
+    },
+    {
+        label: 'Second',
+        value: 2
+    },
+    {
+        label: 'Third',
+        value: 3
+    },
+    {
+        label: 'Forth',
+        value: 4
+    },
+    {
+        label: 'Fifth',
+        value: 5
+    }];
