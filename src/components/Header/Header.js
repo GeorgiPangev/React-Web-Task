ba0aed5457bbe610e8535a429a9eee61c15292e5
PangevG <PangevG@gmail.com>
2022-11-24 20:01:03 +0200

    feat/ added style to Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
+import "./Header.css"
 import {Link} from "react-router-dom";
 
 const Header = () => {
-
     return (
         <header>
             <div className="header-wrapper">
@@ -15,5 +15,4 @@ const Header = () => {
         </header>
     );
 };
-
 export default Header
